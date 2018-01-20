dc96fdd24638e05276f50ce9ccdee5768517c51d
Mikko Pyykkö <mipyykko@gmail.com>
2018-01-20 23:08:14 +0200

    välicommit 1.8-1.9

--- viikko1/unicafe/src/index.js
+++ viikko1/unicafe/src/index.js
@@ -1,6 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Button = ({ handleClick, title }) => {
+    return (
+        <button onClick={handleClick}>{title}</button>
+    )
+}
+
+const Statistic = ({ title, value }) => {
+    return (
+        <li>{title} {value}</li>
+    )
+}
+
+const Statistics = (props) => {
+    const maara = () => props.state.hyva + props.state.neutraali + props.state.huono
+    const keskiarvo = () => maara() === 0 ? 0 : (props.state.hyva - props.state.huono) / maara()
+    const positiiviset = () => maara() === 0 ? 0 : props.state.hyva / maara() * 100 + '%'
+    
+    if (maara() === 0) {
+        return (
+            <div>
+                <h1>statistiikka</h1>
+                <p>ei yhtään palautetta annettu</p>
+            </div>
+        )
+    }
+    
+    return (
+        <div>
+            <h1>statistiikka</h1>
+            <ul>
+                <Statistic title="hyvä" value={props.state.hyva} />
+                <Statistic title="neutraali" value={props.state.neutraali} />
+                <Statistic title="huono" value={props.state.huono} />
+                <Statistic title="keskiarvo" value={keskiarvo()} />
+                <Statistic title="positiiviset" value={positiiviset()} />
+            </ul>
+        </div>
+    )
+}
 class App extends React.Component {
 
     constructor(props) {
@@ -11,6 +50,7 @@ class App extends React.Component {
             huono: 0
         })
     }
+
 
     lisaaHyva = () => {
         return () => {
@@ -31,28 +71,16 @@ class App extends React.Component {
     }
 
     render() {
-        const maara = () => this.state.hyva + this.state.neutraali + this.state.huono
-        const keskiarvo = () => maara() === 0 ? 0 : (this.state.hyva - this.state.huono) / maara()
-        const positiiviset = () => maara() === 0 ? 0 : this.state.hyva / maara() * 100
         
         return (
             <div>
                 <h1>anna palautetta</h1>
                 <div>
-                    <button onClick={this.lisaaHyva()}>hyvä</button>
-                    <button onClick={this.lisaaNeutraali()}>neutraali</button>
-                    <button onClick={this.lisaaHuono()}>huono</button>
+                    <Button handleClick={this.lisaaHyva()} title="hyvä" />
+                    <Button handleClick={this.lisaaNeutraali()} title="neutraali" />
+                    <Button handleClick={this.lisaaHuono()} title="huono" />
                 </div>
-                <h1>statistiikka</h1>
-                <div>
-                    <ul style={{ listStyleType: 'none' }}>
-                        <li>hyvä {this.state.hyva}</li>
-                        <li>neutraali {this.state.neutraali}</li>
-                        <li>huono {this.state.huono}</li>
-                        <li>keskiarvo {keskiarvo()}</li>
-                        <li>positiiviset {positiiviset()}%</li>
-                    </ul>
-                </div>        
+                <Statistics state={this.state} />
             </div>
         )
     }
